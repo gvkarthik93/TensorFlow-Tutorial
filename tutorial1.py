f73b8b14baa269529d624fdbbdffc5c116b200b4
Venkata Karthik Gullapalli <gvkarthik93@ymail.com>
2017-08-03 17:25:20 +0530

    Committing TensorFlow basic

--- tutorial1.py
+++ tutorial1.py
@@ -1,9 +1,14 @@
 import tensorflow as tf
+
+# Creating two source operations a, b that will output two constants
 a = tf.constant([1])
 b = tf.constant([2])
 
-# Adding two tensorflow consants
+# Adding two tensorflow source operations
+# Prints 3
 c = a+b
+
+# Prints 3
 d = tf.add(a,b)
 
 # Creating a TensorFlow Session
